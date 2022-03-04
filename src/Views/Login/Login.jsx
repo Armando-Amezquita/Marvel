79820af6c282c830c53ff5a8e1f38f3298395ce5
Armando Amezquita <a.amezquita.1009@gmail.com>
2022-03-04 10:54:06 -0500

    Formulario de login completo

--- src/Views/Login/Login.jsx
+++ src/Views/Login/Login.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
 import styles from './Login.module.css';
+import { Link } from 'react-router-dom';
+// import Header from '../Header/Header';
 
 export default function Login () {
     return (
-        <div>
-            Login
-        </div>
+        <>
+        <main className={styles.containerMain}>
+            <div className={styles.containerSections}>
+                <section className={styles.containerLeft}>
+                    <p className={styles.logo}>Logo</p>
+                    <form className={styles.containerForm}>
+                        <h1 className={styles.titleForm}> ¡Ingresa a tu cuenta! </h1>
+                        <div className={styles.containerButtonsSocial}>
+                            <button type='submit' className={styles.buttonGoogle}>
+                                <span> </span> Iniciar con Google
+                            </button>
+                            <button type='submit' className={styles.buttonMeta}>
+                                Iniciar con Facebook
+                            </button>
+                        </div>
+                        <label htmlFor="email" className={styles.labels}>
+                            <span className={styles.text}>Correo Electrónico</span>
+                            <input placeholder='correo@gmail.com' id='email' type="text" name='email' className={styles.inputForm} />
+                        </label>
+                        <label htmlFor="password" className={styles.labels}>
+                            <span className={styles.text}>Contraseña</span>
+                            <input placeholder='Tu contraseña' id='password' type="password" name='password' className={styles.inputForm} />
+                        </label>
+                        <div className={styles.containerButtons}>
+                            <button type='submit' className={styles.iniciarSesion}>
+                                Iniciar sesión 
+                            </button>
+                            <button type='submit' className={styles.crearUsuario}>
+                                Crear cuenta
+                            </button>
+                        </div>
+                        <span className={styles.spanRecuperar}>
+                            ¿No recuerdas tu contraseña? 
+                            <Link to='#' className={styles.link}>
+                                <p>Olvide contraseña</p>
+                            </Link>
+                        </span>
+                    </form>
+                </section>
+                <section className={styles.containerRigth}>
+                    {/* <img className={styles.image} src="../../Images/pexels-alex-knight-2599244.jpg" alt="Imagen" /> */}
+                    <p className={styles.image}></p>
+                </section>
+            </div>
+        </main>
+        </>
     )
 }
